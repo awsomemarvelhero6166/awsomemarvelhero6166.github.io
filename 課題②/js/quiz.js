782723bdf17cbfbb90643b72073514ef47d1c160
awsomemarvelhero6166 <68671800+awsomemarvelhero6166@users.noreply.github.com>
2020-07-30 00:05:01 +0900

    Add files via upload

---
+++ 課題②/js/quiz.js
@@ -0,0 +1,44 @@
+function quiz1(push) {
+     
+    var message = "" ;
+    
+    if(push == 1) {
+        message = "You are wrong! Go to hell!";
+    } else if(push == 2) {
+        message = "正解! I love you!";
+    } else if(push == 3) {
+        message = "You are wrong! Go to hell";
+    }
+    
+    alert(message);
+}
+
+function quiz2(push) {
+     
+    var message = "" ;
+    
+    if(push == 1) {
+        message = "You are wrong! Go to hell!";
+    } else if(push == 2) {
+        message = "正解! I love you!";
+    } else if(push == 3) {
+        message = "You are wrong! Go to hell";
+    }
+    
+    alert(message);
+}
+
+function quiz3(push) {
+     
+    var message = "" ;
+    
+    if(push == 1) {
+        message = "You are wrong! Go to hell!";
+    } else if(push == 2) {
+        message = "You are wrong! Go to hell!";
+    } else if(push == 3) {
+        message = "正解！簡単すぎたかな？";
+    }
+    
+    alert(message);
+}
